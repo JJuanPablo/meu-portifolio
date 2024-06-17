8c2787f38b53b6e1cf6a6428894d5a1b2859bfb7
Juan Pablo <159589819+JJuanPablo@users.noreply.github.com>
2024-06-17 12:58:10 -0300

    fix: arrumado problema de não acessar as telas

--- Script/script.js
+++ Script/script.js
@@ -1,24 +1,24 @@
 // Função para redirecionar o usuário para a página de "Materias" de Automação Industrial
 function atentrar(){
-    window.location.href = "Materias/indexat.html";
+    window.location.href = "../Materias/indexat.html";
 }
 
 // Função para redirecionar o usuário para a página de "Materias" de Linguagem da Programação
 function lentrar(){
-    window.location.href = "Materias/indexl.html";
+    window.location.href = "../Materias/indexl.html";
 }
 
 // Função para redirecionar o usuário para a página de "Materias" de Ciências de Dados
 function centrar(){
-    window.location.href = "Materias/indexc.html";
+    window.location.href = "../Materias/indexc.html";
 }
 
 // Função para redirecionar o usuário para a página de "Materias" de Desenvolvimento Web
 function dentrar(){
-    window.location.href = "Materias/indexd.html";
+    window.location.href = "../Materias/indexd.html";
 }
 
 // Função para redirecionar o usuário para a página de "Materias" de Banco de dados
 function bentrar(){
-    window.location.href = "Materias/indexb.html";
+    window.location.href = "../Materias/indexb.html";
 }
