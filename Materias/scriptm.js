e3ba5e1a1cd9dc5c0e5542bce0c8a65d326aeae4
Juan <juanpinpa50@gmail.com>
2024-06-17 01:30:28 -0300

    fisrt commit

---
+++ Materias/scriptm.js
@@ -0,0 +1,24 @@
+// Função para redirecionar o usuário para a página de "Materias" de Automação Industrial
+function atentrar(){
+    window.location.href = "indexat.html"
+}
+
+// Função para redirecionar o usuário para a página de "Materias" de Linguagem da Programação
+function lentrar(){
+    window.location.href = "indexl.html"
+}
+
+// Função para redirecionar o usuário para a página de "Materias" de Ciências de Dados
+function centrar(){
+    window.location.href = "indexc.html"
+}
+
+// Função para redirecionar o usuário para a página de "Materias" de Desenvolvimento Web
+function dentrar(){
+    window.location.href = "indexd.html"
+}
+
+// Função para redirecionar o usuário para a página de "Materias" de Banco de dados
+function bentrar(){
+    window.location.href = "indexb.html"
+}
